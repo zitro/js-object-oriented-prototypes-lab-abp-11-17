d29db7155e84067414c2348b08aa7dc2286cb3cd
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-09 23:43:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,9 @@ BoardMember.prototype.doCharity = function(){
  }
 
 BoardMember.prototype.releasePressStatement = function(){
-return "You will see great things from Scuber."
+return "You will see great things from Scuber.";
 }
 //
 BoardMember.prototype.sayHi = function(){
 return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-}
 }
