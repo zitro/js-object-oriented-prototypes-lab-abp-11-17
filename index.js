e0145a261c12e21054070a12f2aaaaa6da5de9a5
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-09 23:42:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+}
+BoardMember.prototype.veto = function(){
+    return 'No, I must disagree'
+  }
+BoardMember.prototype.approve = function(){
+    return 'You can do that!'
+ }
+BoardMember.prototype.doCharity = function(){
+    return "I like to help people."
+ }
+
+BoardMember.prototype.releasePressStatement = function(){
+return "You will see great things from Scuber."
+}
 //
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+BoardMember.prototype.sayHi = function(){
+return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
+}
